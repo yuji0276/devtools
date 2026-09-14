71e1f1a5c8f56cc21df4fab11ca4afa4b80df00b
yuji0276 <yujikami0110@gmail.com>
2026-09-14 13:27:05 +0900

    Git連携を追加

---
+++ nvim/lua/plugins/git_file.lua
@@ -0,0 +1,5 @@
+return {
+  "benomahony/oil-git.nvim",
+  dependencies = { "stevearc/oil.nvim" },
+  -- No opts or config needed! Works automatically
+}
---
+++ nvim/lua/plugins/git_line.lua
@@ -0,0 +1,6 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  --@module 'gitsigns'
+  --@type gitsigns.SetupOpts
+  opts = {}
+}
