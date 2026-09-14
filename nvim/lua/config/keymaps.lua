ba975f8eccef6195bf793a58b39e26b5b6d46145
yuji0276 <yujikami0110@gmail.com>
2026-09-14 18:05:48 +0900

    エラーメッセージのポップアップを表示するように変更

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,5 @@ map("n", "-", "<Cmd>Oil<CR>", { desc = "親ディレクトリを開く" })
 map("n", "<leader>f", function()
   require("fzf-lua").files()
 end, { desc = "ファイル検索" })
+
+map("n","<leader>e", "<Cmd>:lua vim.diagnostic.open_float()<CR>",{desc = "カーソル行のエラーメッセージをポップアップで表示する"})
